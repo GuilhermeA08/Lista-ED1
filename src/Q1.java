47dbdae6f6b183494e0d7ab353b9923279273ac8
GuilhermeA08 <guilhermesaldanha08@gmail.com>
2022-02-19 18:30:25 -0300

    feat: Questão 1

---
+++ src/Q1.java
@@ -0,0 +1,12 @@
+public class Q1 {
+    public static void main(String[] args) throws Exception {
+        int idade = 21;
+        float altura = 1.70f;
+        char letra = 'G'; 
+        String nome = "Guilherme";
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(letra);
+        System.out.println(nome);
+    }
+}
