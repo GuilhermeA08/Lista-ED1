bc00f512cea6fd4d02941ddd3e1a69138d6a793a
GuilhermeA08 <guilhermesaldanha08@gmail.com>
2022-03-10 17:00:22 -0300

    Questão 9

--- src/Q9.java
+++ src/Q9.java
@@ -1,18 +1,17 @@
+import java.lang.Math;
 import java.util.Scanner;
 
 public class Q9 {
 
   public static void main(String[] args) throws Exception {
-    float altura, peso;
+    float raio;
     try (Scanner ler = new Scanner(System.in)) {
-      System.out.print("Altura: ");
-      altura = ler.nextFloat();
-      System.out.print("Peso: ");
-      peso = ler.nextFloat();
+      System.out.print("raio: ");
+      raio = ler.nextFloat();
     }
-    if (altura != 0) {
-      float IMC = (peso) / (altura * altura);
-      System.out.println(IMC);
+    if (raio > 0) {
+      double area = Math.PI * (raio * raio);
+      System.out.println(area);
     } else {
       System.out.println("Divisão por 0!!!");
     }
