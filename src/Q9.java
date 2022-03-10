44b82578d459c3ff2dc28df2d1001f3ee5c6c24f
GuilhermeA08 <guilhermesaldanha08@gmail.com>
2022-03-10 15:38:11 -0300

    Questão 9

---
+++ src/Q9.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Q9 {
+
+  public static void main(String[] args) throws Exception {
+    float altura, peso;
+    try (Scanner ler = new Scanner(System.in)) {
+      System.out.print("Altura: ");
+      altura = ler.nextFloat();
+      System.out.print("Peso: ");
+      peso = ler.nextFloat();
+    }
+    if (altura != 0) {
+      float IMC = (peso) / (altura * altura);
+      System.out.println(IMC);
+    } else {
+      System.out.println("Divisão por 0!!!");
+    }
+  }
+}
