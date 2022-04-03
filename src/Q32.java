d64ad102a909cd9cacf28c29431cfe9d50ed84a6
GuilhermeA08 <guilhermesaldanha08@gmail.com>
2022-04-03 15:48:18 -0300

    feat: Questão 32

---
+++ src/Q32.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Q32 {
+
+  static Scanner ler = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    String palavra = ler.next();
+    while (palavra.length() > 0) {
+      char letra = palavra.charAt(0);
+      palavra = palavra.substring(1, palavra.length());
+
+      int count = 1;
+      for (int i = 0; i < palavra.length(); i++) {
+        if (palavra.charAt(i) == letra) {
+          count++;
+        }
+      }
+      palavra = palavra.replaceAll("" + letra, "");
+      System.out.print(" " + letra + ": " + count);
+    }
+    System.out.println();
+  }
+
+  public static void contar() {}
+
+  public static void exibir() {}
+}
